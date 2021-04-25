c08270d11535a370f4b41c7e72729b615d6ac48a
Leonid Kuznetsov <l.kuznetsov@p1k.org>
2021-04-25 22:12:45 +0300

    add critical css

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -1,8 +1,10 @@
 // development config
 const { merge } = require("webpack-merge");
+const { resolve } = require("path");
 const webpack = require("webpack");
 const commonConfig = require("./common");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
 
 module.exports = merge(commonConfig, {
   mode: "development",
--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -1,7 +1,9 @@
 // production config
 const { merge } = require("webpack-merge");
+const { resolve } = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
 
 const commonConfig = require("./common");
 
@@ -13,6 +15,19 @@ module.exports = merge(commonConfig, {
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css",
+    }),
+    new HtmlCriticalWebpackPlugin({
+      base: resolve(__dirname, "../../dist"),
+      src: "index.html",
+      dest: "index.html",
+      inline: true,
+      minify: true,
+      extract: true,
+      width: 375,
+      height: 565,
+      penthouse: {
+        blockJSRequests: false,
+      },
     }),
   ],
   module: {
