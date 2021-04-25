fa2820d2bb7a98f678d33feb30d98989b98bb6de
Leonid Kuznetsov <l.kuznetsov@p1k.org>
2021-04-25 22:26:52 +0300

    refactor code

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,30 +7,19 @@ import {
 } from "react-router-dom";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import Exchange from "../pages/Exchange/Exchange";
-import { Spinner } from "../components/Spinner/Spinner";
-
-import styles from "../App.module.scss";
 
 export const ApplicationRouter = (): JSX.Element => {
   return (
     <Router>
-      <Suspense
-        fallback={
-          <div className={styles.LoadingWrapper}>
-            <Spinner />
-          </div>
-        }
-      >
-        <Switch>
-          <Route path="/dashboard/:id?" exact>
-            <Dashboard />
-          </Route>
-          <Route path="/account/:id" exact>
-            <Exchange />
-          </Route>
-          <Redirect from={"/"} to={"/dashboard"} />
-        </Switch>
-      </Suspense>
+      <Switch>
+        <Route path="/dashboard/:id?" exact>
+          <Dashboard />
+        </Route>
+        <Route path="/account/:id" exact>
+          <Exchange />
+        </Route>
+        <Redirect from={"/"} to={"/dashboard"} />
+      </Switch>
     </Router>
   );
 };
